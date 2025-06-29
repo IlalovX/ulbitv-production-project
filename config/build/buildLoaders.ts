66047f9d10a6530f24d78add6c64f9c2ba7d0d97
IlalovX <ilalovx@gmail.com>
2025-06-30 02:39:04 +0500

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,31 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { RuleSetRule } from 'webpack'
+import { BuildOptions } from './types/config'
+
+export function buildLoaders(options: BuildOptions): RuleSetRule[] {
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						localIdentName: options.isDev
+							? '[path][name]__[local]'
+							: '[hash:base64:8]',
+					},
+				},
+			},
+			'sass-loader',
+		],
+	}
+
+	const tsLoader = {
+		test: /\.tsx?$/,
+		use: 'ts-loader',
+		exclude: /node_modules/,
+	}
+	return [tsLoader, cssLoader]
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import { Configuration } from 'webpack'
+import { buildDevServer } from './buildDevServer'
+import { buildLoaders } from './buildLoaders'
+import { buildPlugins } from './buildPlugins'
+import { buildResolvers } from './buildResolvers'
+import { BuildOptions } from './types/config'
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+	return {
+		mode: options.mode,
+		resolve: buildResolvers(),
+		entry: options.paths.entry,
+		output: {
+			filename: '[name].[contenthash].js',
+			path: options.paths.build,
+			clean: true,
+		},
+		plugins: buildPlugins(options),
+		module: {
+			rules: buildLoaders(options),
+		},
+		devtool: options.isDev ? 'inline-source-map' : undefined,
+		devServer: options.isDev ? buildDevServer(options) : undefined,
+	}
+}
